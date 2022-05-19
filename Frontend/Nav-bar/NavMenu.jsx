f6647e59ed6eddbd59edd0876c26585fec4afa59
Nraxi <olle.code@hotmail.com>
2022-05-19 18:55:00 +0200

    extra inlagt på nav

--- Frontend/Nav-bar/NavMenu.jsx
+++ Frontend/Nav-bar/NavMenu.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { Link } from "react-router-dom";
 import "../scss/style.scss";
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 
 function NavMenu() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar fixed="top" bg="dark" variant="dark" expand="lg" className="mb-4">
       <Container>
         <Navbar.Brand href="/">Byn-Blocket</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/shop">Shop</Nav.Link>
           <Nav.Link href="/varukorg">Varukorg</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
