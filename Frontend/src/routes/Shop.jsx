a724d3532558de957183238a9f50055a2394ab92
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-05-20 00:56:10 +0200

    Lite frontend på produkt sida

--- Frontend/src/routes/Shop.jsx
+++ Frontend/src/routes/Shop.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function Shop() {
   return (
     <div>
       <h3>hej shop</h3>Shop
+
+      <Link to="/Produktsida"><h3>TEST PRODUCT</h3></Link>
     </div>
   )
 }
