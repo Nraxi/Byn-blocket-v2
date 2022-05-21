6e3d5ec38ef865b4e2d9299b8b9717899220b4be
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-05-22 00:11:55 +0200

    mer frontend jobb

--- Frontend/src/routes/Produktsida.jsx
+++ Frontend/src/routes/Produktsida.jsx
@@ -1,25 +1,49 @@
 import React from 'react'
 import mobile from '../img/mobile.jpeg';
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, Container, Form } from 'react-bootstrap'
+import "../../scss/style.scss";
+
 function Produktsida() {
   return (
-    <div>
-      <Card>
-        <Card.Title>Produktsida </Card.Title>
-        <Card.Text>3999:-</Card.Text>
+    <Container>
+      <Form className='produkt-form' >
+        <h1>Mobile phone</h1>
+        <h2>3999:-</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <label>Kapacitet</label>
+      </Form>
+      <select className='gb-option'>
+        <option>64GB</option>
+        <option>128GB</option>
+      </select>
+      <Button className='buy' variant='success'>LÄGG I VARUKORG</Button>
+      <img
+        className='mobile-phone'
+        src={mobile}
+        alt='mobile phone' />
+    </Container>
 
-        <Card.Img variant='top' src={mobile} style={{ width: '18rem' }} alt='mobile phone' />
-      </Card>
-      <Card>
-        <Card.Body>Lorem ipsum</Card.Body>
-      </Card>
-      <Button variant='success'>LÄGG I VARUKORG</Button>
 
 
 
 
 
-    </div>
+
+
+
+
+    // <div>
+    //   <Card>
+    //     <Card.Title>Produktsida </Card.Title>
+    //     <Card.Text>3999:-</Card.Text>
+
+    //     <Card.Img variant='top' src={mobile} style={{ width: '18rem' }} alt='mobile phone' />
+    //   </Card>
+    //   <Card>
+    //     <Card.Body>Lorem ipsum</Card.Body>
+    //   </Card>
+
+    // </div>
 
   )
 }
